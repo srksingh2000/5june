358572589bec204b3b7b80b1f1848612483d15bb
Nishant Singh <755083@student.tor.loc>
2019-06-05 13:50:55 -0400

    second commit

---
+++ nishant.js
@@ -0,0 +1,25 @@
+var a = 6;
+
+function test() {
+
+    vara = 7;
+
+    function again() {
+
+
+
+        var a = 8;
+        alert(a); //A
+
+    }
+
+    again();
+    alert(a); //B
+
+
+
+
+}
+
+test();
+alert(a); //c
